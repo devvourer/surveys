bb3fda435f994ee4153ed90de3bc0f912c164892
devvourer <beksultan.toktobekov@mail.ru>
2021-09-12 22:37:18 +0600

    fix urls

--- surveys/urls.py
+++ surveys/urls.py
@@ -1,20 +1,6 @@
 from django.urls import path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 
 from .views import SurveyViewSet, QuestionViewSet, ChoiceViewSet, AnswerViewSet, login
-
-
-# api
-schema_view = get_schema_view(
-    openapi.Info(
-        title='Surveys API',
-        default_version='v1',
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 # survey views
 survey_list = SurveyViewSet.as_view({
@@ -63,9 +49,4 @@ urlpatterns = [
     path('answers/create/', answer_create, name='answer_create'),
     path('answers/', answer_view, name='answer_view'),
     path('login/', login, name='login'),
-
-    # API
-    path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
